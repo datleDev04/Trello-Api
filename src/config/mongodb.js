9b105883e1ebce7d8440ac2a9f25f48ab39d68f4
letiendat040904@gmail.com <letiendat040904@gmail.com>
2024-02-21 11:32:05 +0700

    get db password

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,32 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+const MONGODB_URI = 'mongodb+srv://letiendat040904:<password>@trello-api.per40n5.mongodb.net/?retryWrites=true&w=majority'
+
+const DATABASE_NAME = 'Trello-API'
+
+const { MongoClient, ServerApiVersion } = require('mongodb')
+
+let trelloDatabaseInstance = null
+
+const clientDatabaseInstance = new MongoClient(MONGODB_URI, {
+  // dành cho mongodb server 5.0 or later
+  // là 1 sự lựa chọn optional
+  // đảm bảo tính tương thích và duy trì ổn định khi MongoDb được cập nhật lên các phiên bản mới
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+// kết nối với database
+export const CONNECT_DB = async () => {
+  // kết nối với mongodb Atlas
+  await clientDatabaseInstance.connect()
+  // lưu giá trị của database DATABASE_NAME cho biến trelloDatabaseInstance
+  trelloDatabaseInstance = clientDatabaseInstance.db(DATABASE_NAME)
+}
+
+// export là trelloDatabaseInstance sau khi connect thành công
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to mongodb first')
+  return trelloDatabaseInstance
+}
--- src/server.js
+++ src/server.js
@@ -1,32 +1,34 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+/* eslint-disable no-console */
+
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import { CONNECT_DB, GET_DB } from './config/mongodb'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  const hostname = 'localhost'
+  const port = 8017
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.get('/', (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
+  })
+}
+
+(async () => {
+  try {
+    console.log('Connecting to mongodb...')
+    await CONNECT_DB()
+    console.log('Connected to mongodb...')
+    START_SERVER()
+  } catch (error) {
+    console.log(error)
+    process.exit(0)
+  }
+
+})()
