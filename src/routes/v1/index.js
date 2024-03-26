063681fa7ae43007ab901370b0d717159c33c8e7
letiendat040904@gmail.com <letiendat040904@gmail.com>
2024-03-26 16:09:36 +0700

    finish API_create_column

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import { StatusCodes } from 'http-status-codes'
 
 import { boardRoute } from './boardRoute'
+import columnRoute from './columnRoute'
+import cardRoute from './cardRoute'
 
 const Router = express.Router()
 
@@ -12,5 +14,11 @@ Router.get('/status', (req, res) => {
 
 // Board route
 Router.use('/board', boardRoute)
+
+// column route
+Router.use('/column', columnRoute)
+
+// card route
+Router.use('/card', cardRoute)
 
 export const APIs_V1 = Router
