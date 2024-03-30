e66109ba814a134c5a2b10346bf792704ff04f59
letiendat040904@gmail.com <letiendat040904@gmail.com>
2024-03-30 08:33:37 +0700

    deploy-updateWhiteListDomain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,7 @@ export const API_ROOT = 'http://localhost:8017'
 
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
+  'https://trello-web-self.vercel.app/'
 ]
 
 export const BOARD_TYPES = {
